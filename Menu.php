6ee93dbb9996a12eb94dc5415b3b8f1750184900
DanielMarujo05 <138403956+DanielMarujo05@users.noreply.github.com>
2024-09-10 19:18:58 -0300

    Update Menu.php

---
+++ Menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menu disciplinas</title>
+</head>
+ 
+<body>
+<li>  
+<ul><a href="~/Disciplinas/IncluirDisciplina.php">Incluir disciplina</a></ul>
+<ul><a href="~/Disciplinas/ListarUmaOuTodas.php">Listar Disciplinas </a></ul>
+<ul><a href="~/Disciplinas/alterarDisciplina.php">Alterar Disciplina </a></ul>
+<ul><a href="~/Disciplinas/ExcluirDisciplina.php">Excluir Disciplina </a></ul>
+</li>
+</body>
+</html>
